754dac6028d4180dff072067b4614d236c79b69f
Ven Meyerzon <vmmeyerzon@ucdavis.edu>
2020-11-03 21:40:11 -0800

    Rebuilt to add style tag within page

--- makeDestinyTestBlue.user.js
+++ makeDestinyTestBlue.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Make Destiny One Test Blue
 // @namespace    https://github.com/venmey/Destiny-One-TamperMonkey-Scripts
-// @version      1.02
+// @version      2.0
 // @description  Makes various elements in Destiny One TEST blue instead of red to distinguish it from PROD.
 // @author       Ven Meyerzon
 // @match        https://ucdavistestsv.destinysolutions.com/srs/*
@@ -11,11 +11,31 @@
 (function() {
     'use strict';
 
-    let sheet = window.document.styleSheets[9];
-    sheet.insertRule(
-        '#navigation01, #footer01, #navigation02 ul.navigationMenu > li:hover, .dropDownThirdLevelMenu > li > div:hover, .dropDownThirdLevelMenu > li > div:hover, #main-menu ul li.hover li.hover { background-color: #1d22b5; }',
-        sheet.cssRules.length); // main nav, footer, secondary nav dropdown, teritary menu item, hover over main nav dropdown to blue
-    sheet.insertRule('#main-menu ul li.selectedItem:hover { background-color: #2e30d9; }', sheet.cssRules.length); // top-level dropdown navigation to blue
-    sheet.insertRule('.dropDownThirdLevelMenu li.selected > div a { border-left: 5px solid #1d22b5; }', sheet.cssRules.length); // teriary menu currently selected page marker to blue
-    sheet.insertRule('#navigation02 ul.navigationMenu > li.selected > div > a, .dropDownThirdLevelMenu li.selected > div a { color: #1d22b5; }', sheet.cssRules.length); // secondary selected menu, teriary selected menu font color to blue
+    document.head.insertAdjacentHTML("beforeend",
+ `<style>
+ #navigation01, #footer01, #navigation02 ul.navigationMenu > li:hover, .dropDownThirdLevelMenu > li > div:hover, .dropDownThirdLevelMenu > li > div:hover, #main-menu ul li.hover li.hover {
+     background-color: #1d22b5 !important;
+ }
+
+ #main-menu ul li.selectedItem:hover {
+     background-color: #2e30d9 !important;
+ }
+
+ .dropDownThirdLevelMenu li.selected > div a {
+     border-left: 5px solid #1d22b5 !important;
+ }
+
+ #navigation02 ul.navigationMenu > li.selected > div > a, .dropDownThirdLevelMenu li.selected > div a {
+     color: #1d22b5 !important;
+ }
+
+ .dropDownThirdLevelMenu li > div:hover > a, #navigation02 ul.navigationMenu > li:hover > div > a {
+     color: #ffffff !important;
+ }
+
+ .schoolName-text {
+    font-size: 12pt !important;
+ }
+ </style>`
+)
 })();
