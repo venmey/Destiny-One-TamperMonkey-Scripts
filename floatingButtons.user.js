78051e3c5fdf41631a24e3a82084b0e09ec2e8eb
Ven Meyerzon <vmmeyerzon@ucdavis.edu>
2020-11-04 22:18:04 -0800

    Updated to include #save buttons, too.

--- floatingButtons.user.js
+++ floatingButtons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Floating Buttons
 // @namespace    https://github.com/venmey/Destiny-One-TamperMonkey-Scripts
-// @version      1.0
+// @version      1.1
 // @description  Makes the Save button on various pages float on the left side of the screen as you scroll for easier access.
 // @author       Ven Meyerzon
 // @match        https://ucdavistestsv.destinysolutions.com/srs/*
@@ -11,11 +11,11 @@
 (function() {
   'use strict';
   
-  let saveButton = document.getElementById("savePage");
+  let saveButton = document.querySelectorAll("#savePage, #save");
   
-  if (saveButton) {
-	  let cloneButton = saveButton.cloneNode(true);
-	  saveButton.insertAdjacentElement('beforebegin', cloneButton)
+  if (saveButton[0]) {
+	  let cloneButton = saveButton[0].cloneNode(true);
+	  saveButton[0].insertAdjacentElement('beforebegin', cloneButton)
 	  cloneButton.style.cssText = "position: fixed; left: 17%;  top: 93%; font-size: 20px !important;"
   }
 
