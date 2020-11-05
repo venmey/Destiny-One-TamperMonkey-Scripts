8ad55bc190192942cfd310340508b85f8cf5cd71
Ven Meyerzon <vmmeyerzon@ucdavis.edu>
2020-11-04 20:29:38 -0800

    Updated blues.

--- makeDestinyTestBlue.user.js
+++ makeDestinyTestBlue.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Make Destiny One Test Blue
 // @namespace    https://github.com/venmey/Destiny-One-TamperMonkey-Scripts
-// @version      2.3
+// @version      2.4
 // @description  Makes various elements in Destiny One TEST blue instead of red to distinguish it from PROD.
 // @author       Ven Meyerzon
 // @match        https://ucdavistestsv.destinysolutions.com/srs/*
@@ -14,7 +14,7 @@
     document.head.insertAdjacentHTML("beforeend",
  `<style>
  #navigation01, #footer01, #navigation02 ul.navigationMenu > li:hover, .dropDownThirdLevelMenu > li > div:hover, .dropDownThirdLevelMenu > li > div:hover, #main-menu ul li.hover li.hover {
-     background-color: #1D4776 !important;
+     background-color: #4F7094 !important;
  }
 
  #main-menu ul li.selectedItem:hover {
@@ -22,11 +22,11 @@
  }
 
  .dropDownThirdLevelMenu li.selected > div a {
-     border-left: 5px solid #1D4776 !important;
+     border-left: 5px solid #4F7094 !important;
  }
 
  #navigation02 ul.navigationMenu > li.selected > div > a, .dropDownThirdLevelMenu li.selected > div a {
-     color: #1D4776 !important;
+     color: #4F7094 !important;
  }
 
  .dropDownThirdLevelMenu li > div:hover > a, #navigation02 ul.navigationMenu > li:hover > div > a {
