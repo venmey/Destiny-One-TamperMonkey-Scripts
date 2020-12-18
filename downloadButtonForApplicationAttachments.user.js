cc20d936b6108ac3b1c8a71b622fcc820d32cf3d
Ven Meyerzon <vmmeyerzon@ucdavis.edu>
2020-12-17 21:59:01 -0800

    Fixed error when no attachments present.

--- downloadButtonForApplicationAttachments.user.js
+++ downloadButtonForApplicationAttachments.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [D1] Download Button for Application Attachments
 // @namespace    https://github.com/venmey/Destiny-One-TamperMonkey-Scripts
-// @version      2.1
+// @version      2.2
 // @description  Adds a download button next to each attached file in a student's application for faster saving of application attachments.
 // @author       Ven Meyerzon
 // @match        https://ucdavistestsv.destinysolutions.com/srs/enrolmgr/student/applications/*
@@ -13,7 +13,7 @@
     'use strict';
 
     // function that will attempt to download all URLs in an array
-    window.downloadAll = function(urls) {
+    function downloadAll(urls) {
         for (let i = 0; i < urls.length; i++) {
             let a = document.createElement("a");
             a.setAttribute("href", urls[i][0]);
@@ -29,45 +29,48 @@
     // Select all the hyperlinks within the #uploadFile table
     let attachments = document.querySelectorAll("#uploadFile tr a");
 
-    // Loop through the nodelist of hyperlinks
-    attachments.forEach(function(attachment) {
-        // Regex to match url of file
-        let urlRegex = /centeredPopup\('(.*)'/; 
+    if (attachments.length > 0) {
 
-        // Get onclick attribute of hyperlink since it contains the url to the file
-        let onClickContents = attachment.getAttribute("onclick");
+        // Loop through the nodelist of hyperlinks
+        attachments.forEach(function(attachment) {
+            // Regex to match url of file
+            let urlRegex = /centeredPopup\('(.*)'/; 
 
-        // Use the regex to pull out the URL to the file
-        let url = onClickContents.match(urlRegex)[1];
+            // Get onclick attribute of hyperlink since it contains the url to the file
+            let onClickContents = attachment.getAttribute("onclick");
 
-        // Get the file name
-        let fileName = attachment.innerText;
+            // Use the regex to pull out the URL to the file
+            let url = onClickContents.match(urlRegex)[1];
 
-        // Add url and file name to urlArray
-        urlArray.push([url, fileName]);
-        
-        // Create a new node that is a hyperlink to the file with a download button image
-        let downloadButton = document.createElement("a");
-        downloadButton.setAttribute("href", url);
-        downloadButton.setAttribute("download", fileName);
-        let tooltip = "Download " + fileName;
-        downloadButton.innerHTML = '<img style="width: 20px; margin-bottom: -5px; margin-left: 10px;" src="https://github.com/venmey/Destiny-One-TamperMonkey-Scripts/raw/main/images/downloadIcon.png" alt="' + tooltip + '" title="' + tooltip + '">';
+            // Get the file name
+            let fileName = attachment.innerText;
 
-        // Add the new node to the DOM
-        attachment.after(downloadButton);
-    });
+            // Add url and file name to urlArray
+            urlArray.push([url, fileName]);
+            
+            // Create a new node that is a hyperlink to the file with a download button image
+            let downloadButton = document.createElement("a");
+            downloadButton.setAttribute("href", url);
+            downloadButton.setAttribute("download", fileName);
+            let tooltip = "Download " + fileName;
+            downloadButton.innerHTML = '<img style="width: 20px; margin-bottom: -5px; margin-left: 10px;" src="https://github.com/venmey/Destiny-One-TamperMonkey-Scripts/raw/main/images/downloadIcon.png" alt="' + tooltip + '" title="' + tooltip + '">';
 
-    // Create downloadAll button to download all attachments at once
-    let downloadAllButton = document.createElement("button");
-    downloadAllButton.setAttribute("type", "button");
-    downloadAllButton.setAttribute("class", "yui3-button button-intraForm");
-    downloadAllButton.setAttribute("id", "downloadAllButton");
-    downloadAllButton.setAttribute("name", "downloadAllButton");
-    downloadAllButton.setAttribute("value", "Download All");
-    downloadAllButton.setAttribute("title", "Download All Attachments");
-    downloadAllButton.innerText = "Download All";
-    downloadAllButton.addEventListener("click", function() { downloadAll(urlArray); });
+            // Add the new node to the DOM
+            attachment.after(downloadButton);
+        });
 
-    // Place downloadAll button below the "Attached Files" text
-    document.querySelector("#uploadFile").parentElement.previousElementSibling.appendChild(downloadAllButton);
+        // Create downloadAll button to download all attachments at once
+        let downloadAllButton = document.createElement("button");
+        downloadAllButton.setAttribute("type", "button");
+        downloadAllButton.setAttribute("class", "yui3-button button-intraForm");
+        downloadAllButton.setAttribute("id", "downloadAllButton");
+        downloadAllButton.setAttribute("name", "downloadAllButton");
+        downloadAllButton.setAttribute("value", "Download All");
+        downloadAllButton.setAttribute("title", "Download All Attachments");
+        downloadAllButton.innerText = "Download All";
+        downloadAllButton.addEventListener("click", function() { downloadAll(urlArray); });
+
+        // Place downloadAll button below the "Attached Files" text
+        document.querySelector("#uploadFile").parentElement.previousElementSibling.appendChild(downloadAllButton);
+    }
 })();
