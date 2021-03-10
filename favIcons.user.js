a450448c2ffc9b08c0945e584ff7027563db92dc
Ven Meyerzon <vmmeyerzon@ucdavis.edu>
2021-03-09 22:15:51 -0800

    Now replaces existing favIcon.

--- favIcons.user.js
+++ favIcons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [D1] Set Favicons
 // @namespace    https://github.com/venmey/Destiny-One-TamperMonkey-Scripts
-// @version      1.1
+// @version      2.0
 // @description  Sets a favicon for PROD and TEST
 // @author       Ven Meyerzon
 // @match        https://ucdavistestsv.destinysolutions.com/srs/*
@@ -15,12 +15,18 @@
     // Set favIcon URLs
     let prodIcon = 'https://github.com/venmey/Destiny-One-TamperMonkey-Scripts/raw/main/images/prod.ico';
     let testIcon = 'https://github.com/venmey/Destiny-One-TamperMonkey-Scripts/raw/main/images/test.ico';
-
-    // Create favIcon element and add to document
     let favImg = (window.location.href.indexOf('ucdavissv') > -1 ? prodIcon : testIcon);
-    let headTitle = document.querySelector('head');
-    let setFavicon = document.createElement('link');
-    setFavicon.setAttribute('rel', 'shortcut icon');
-    setFavicon.setAttribute('href', favImg);
-    headTitle.appendChild(setFavicon);
+
+    // Check if favIcon already exists.
+    if (document.querySelector("link[rel='shortcut icon']")) {
+        // FavIcon exists, so let's replace it
+        document.querySelector("link[rel='shortcut icon']").href = favImg;
+    } else {
+        // FavIcon doesn't exist, so let's create one
+        let headTitle = document.querySelector('head');
+        let setFavicon = document.createElement('link');
+        setFavicon.setAttribute('rel', 'shortcut icon');
+        setFavicon.setAttribute('href', favImg);
+        headTitle.appendChild(setFavicon);
+    } 
 })();
